c7d15d5f3fb0358fc437f86b795ed8916fbc6a89
Near Bluff <skompc@gmail.com>
2018-04-11 16:05:44 -0500

    fix my dumb mistakes...

--- hold-my-beer.sh
+++ hold-my-beer.sh
@@ -2,13 +2,13 @@ export DIR=`pwd`
 sudo apt install gradle
 sudo apt build-dep wine
 sudo apt build-dep qemu
-sudo apt install x86_64-w64-mingw32-gcc
-sudo apt install i686-w64-mingw32-gcc
+sudo apt install gcc-mingw-w64-x86-64
+sudo apt install gcc-mingw-w64-i686
 mkdir wine-android && cd wine-android
 wget https://dl.winehq.org/wine/source/3.x/wine-3.5.tar.xz
-tar xf wine-3.0.tar.xz
-cp -r wine-3.0 wine-3.0-native
-cd wine-3.0-native
+tar xf wine-3.5.tar.xz
+cp -r wine-3.5 wine-3.5-native
+cd wine-3.5-native
 ./configure --enable-win64
 make
 cd ..
@@ -21,11 +21,11 @@ tar xf freetype-2.6.tar.gz
 cd freetype-2.6
 ./configure --host=$TOOLCHAIN_TRIPLE --prefix=`pwd`/output --without-zlib --with-png=no --with-harfbuzz=no
 make -j`nproc` && make install
-cd ../wine-3.0
+cd ../wine-3.5
 export CFLAGS="-O2"
 export FREETYPE_CFLAGS="-I`pwd`/../freetype-2.6/output/include/freetype2"
 export FREETYPE_LIBS="-L`pwd`/../freetype-2.6/output/lib"
-./configure --host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE --with-wine-tools=../wine-3.0-native --prefix=`pwd`/dlls/wineandroid.drv/assets
+./configure --host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE --with-wine-tools=../wine-3.5-native --prefix=`pwd`/dlls/wineandroid.drv/assets
 make -j`nproc` && make install
 cp ../freetype-2.6/output/lib/libfreetype.so dlls/wineandroid.drv/assets/arm64-v8a/lib/
 cd dlls/wineandroid.drv/
@@ -39,4 +39,4 @@ git submodule update
 make
 cp -r $DIR/hangover/build/qemu/x86_64-windows-user/ $DIR/output/qemu
 cp $DIR/hangover/build/qemu/x86_64-windows-user/qemu-x86_64.exe.so $DIR/output/qemu/qemu.exe
-cp $DIR/wine-3.0/dlls/wineandroid.drv/wine-debug.apk $DIR/output/wine.apk
+cp $DIR/wine-3.5/dlls/wineandroid.drv/wine-debug.apk $DIR/output/wine.apk
